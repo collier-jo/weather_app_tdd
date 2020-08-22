2a63db375575164a6558acd6668083fe861db911
joannacollier <collj035@gmail.com>
2020-08-22 13:23:05 +0100

    Set up test helper, inital html added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <title>Weather App</title>
+        <h1 id="title">Weather App</h1>
       </header>
+      <main>
+        
+      </main>
+
+      
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { mount } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Weather app", () => {
+
+  let wrapper 
+
+  beforeEach(() => {
+    wrapper = mount(<App />)
+  });
+  
+  test('renders title', () => {
+    expect(wrapper.find("#title").text()).toContain("Weather App")
+  });
 });
